1860fd678c58a94565439c44115a357d5631dfdf
aeshwd <aeshgoswamiwd@gmail.com>
2025-01-07 18:59:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const players = [
       name: "Gurman",
       admissionNumber: "ADM102",
       admissionDate: "2024-08-03",
-      feesStatus: "pending",
+      feesStatus: "paid",
       dueDate: "2025-01-03",
       feesHistory: [
         { month: "Aug", year: 2024, status: "paid" },
@@ -26,6 +26,7 @@ const players = [
         { month: "Oct", year: 2024, status: "paid" },
         { month: "Nov", year: 2024, status: "paid" },
         { month: "Dec", year: 2024, status: "paid" },
+        { month: "Jan", year: 2025, status: "paid" },
       ],
     },
 
